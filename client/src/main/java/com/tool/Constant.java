8d65e9526b34a664f3ae64816a9f5c80aca7996e
bzsome <35402969+bzsome@users.noreply.github.com>
2018-06-26 20:56:15 +0800

    更改远程地址路径

--- client/src/main/java/com/tool/Constant.java
+++ client/src/main/java/com/tool/Constant.java
@@ -1,9 +1,9 @@
 package com.tool;
 
 public class Constant {
-    public static final String WEB_URL = "http://localhost:8080/gobang03";
-    public static final String WEB_API_login = WEB_URL + "/webuser/login.action";
+    public static final String WEB_URL = "http://gobang02.bzchao.com/";
+    public static final String WEB_API_login = WEB_URL + "service/webuser/login.action";
     public static final String WEB_URL_admin = WEB_URL + "/admin/";
     public static final String WEB_URL_register = WEB_URL + "/webuser/register.html";
-    public static final String WEB_URL_usercenter = WEB_URL + "/webuser/usercenter.html";
+    public static final String WEB_URL_usercenter = WEB_URL + "/webuser/userCenter.html";
 }
