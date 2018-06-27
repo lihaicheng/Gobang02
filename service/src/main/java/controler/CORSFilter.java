fe423baf0b3a434541dd8bf3bd70fcc1429578ff
bzsome <35402969+bzsome@users.noreply.github.com>
2018-06-28 01:23:18 +0800

    新增过滤器，解决ajax跨域，设置UTF-8编码

---
+++ service/src/main/java/controler/CORSFilter.java
@@ -0,0 +1,44 @@
+package controler;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+@WebFilter("/*")
+public class CORSFilter implements Filter {
+    private final String encoding = "UTF-8";
+
+    public CORSFilter() {
+    }
+
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        //设置跨域请求
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3628800");
+        System.out.println("设置跨域请求");
+
+        setEncoding(req, res);
+
+        chain.doFilter(req, response);
+    }
+
+    public void init(FilterConfig fConfig) throws ServletException {
+    }
+
+    private void setEncoding(ServletRequest req, ServletResponse res) throws UnsupportedEncodingException {
+        HttpServletRequest httpRequest = (HttpServletRequest) req;
+        HttpServletResponse httpResponse = (HttpServletResponse) res;
+
+        httpRequest.setCharacterEncoding(encoding);
+        httpResponse.setCharacterEncoding(encoding);
+
+    }
+}
